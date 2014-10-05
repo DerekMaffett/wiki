087b815688484acf61f85194aaf79d80b8e668a2
Derek Maffett <derekgmaffett@gmail.com>
2014-10-04 21:53:52 -0700

    add devise for user registration

--- Gemfile
+++ Gemfile
@@ -4,12 +4,12 @@ gem 'coffee-rails', '~> 4.0.0'
 gem 'devise'
 gem 'eefgilm'
 gem 'foreman'
+gem 'foundation-rails'
 gem 'guard-bundler'
 gem 'guard-livereload'
 gem 'guard-rails'
 gem 'jquery-rails'
 gem 'rails', '4.1.6'
-gem 'rails_layout'
 gem 'sass-rails', '~> 4.0.3'
 gem 'sdoc', '~> 0.4.0', group: :doc
 gem 'spring', group: :development
@@ -21,4 +21,6 @@ group :development do
   gem 'better_errors'
   gem 'binding_of_caller'
   gem 'spring'
+  gem 'rails_layout'
+  gem 'seed_dump'
 end
--- db/seeds.rb
+++ db/seeds.rb
@@ -1,7 +1,5 @@
-# This file should contain all the record creation needed to seed the database with its default values.
-# The data can then be loaded with the rake db:seed (or created alongside the db with db:setup).
-#
-# Examples:
-#
-#   cities = City.create([{ name: 'Chicago' }, { name: 'Copenhagen' }])
-#   Mayor.create(name: 'Emanuel', city: cities.first)
+Article.create!([
+  {title: "Balboa", body: "Balboa is a dance invented in California when people didn't have space on the dance floor. Related to Lindy Hop, it is generally danced to fast songs. \r\n "},
+  {title: "Lindy Hop", body: "Lindy Hop is a dance from the 40's done to Jazz music at speeds usually between 100 and 200 bpm. "},
+  {title: "Blues", body: "Blues dancing is usually done to much slower music. "}
+])
